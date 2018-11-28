d9501317a03c4f4d808cfe915db942c83c1f02ab
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2018-11-28 21:47:16 -0200

    primeiro commit trabalho de BD

---
+++ tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE usuario (
+    email varchar(255),
+    nome varchar(255)
+);
+
+CREATE TABLE produtos (
+    id int,
+    nome varchar(255),
+    tipo varchar(255),
+    category varchar(255)
+);
+
