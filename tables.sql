4aae07a7da0509ab0148ce0f7b655b7221e5f58d
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2018-11-28 23:16:26 -0200

    usuario & Produtos

--- tables.sql
+++ tables.sql
@@ -1,12 +1,25 @@
 CREATE TABLE usuario (
+    usuarioID int NOT NULL,
+    cpf varchar(255),
     email varchar(255),
-    nome varchar(255)
+    nome varchar(255),
+    carrinhoID int,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE produtos (
-    id int,
+    produtosID int NOT NULL,
     nome varchar(255),
     tipo varchar(255),
-    category varchar(255)
+    category varchar(255),
+    PRIMARY KEY (produtosID),
+    FOREIGN KEY (carrinhoid) REFERENCES usuario(carrinhoID)
 );
+
+CREATE TABLE endereco (
+    
+);
+
+
+
 
