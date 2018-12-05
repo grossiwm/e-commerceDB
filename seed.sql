f5106495b35438048364c1e9f89a05d123a44607
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2018-12-05 21:57:09 -0200

    create tables finalizado, e um insert

---
+++ seed.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO usuario (cpf, email, nome)
+VALUES ('000000000', 'luiz@hotmail.com', 'luiz');
--- tables.sql
+++ tables.sql
@@ -1,24 +1,67 @@
 CREATE TABLE usuario (
-    usuarioID int NOT NULL,
+    usuarioID INTEGER PRIMARY KEY AUTOINCREMENT,
     cpf varchar(255),
     email varchar(255),
-    nome varchar(255),
-    carrinhoID int,
-    PRIMARY KEY (usuarioID)
+    nome varchar(255)
 );
 
-CREATE TABLE produtos (
-    produtosID int NOT NULL,
+CREATE TABLE produto (
+    produtoID int NOT NULL,
     nome varchar(255),
     tipo varchar(255),
     category varchar(255),
-    carrinhoID int,
-    PRIMARY KEY (produtosID),
-    FOREIGN KEY (carrinhoID) REFERENCES usuario(usuarioID)
+    usuarioID int,
+    PRIMARY KEY (produtoID),
+    FOREIGN KEY (usuarioID) REFERENCES usuario(usuarioID)
 );
 
+CREATE TABLE adiciona (
+    quantidade int,
+    produtoID int NOT NULL,
+    usuarioID int NOT NULL,
+    PRIMARY KEY (produtoID, usuarioID),
+    FOREIGN KEY (usuarioID) REFERENCES usuario(usuarioID),
+    FOREIGN KEY (produtoID) REFERENCES produto(produtoID)
+);
 
+CREATE TABLE endereco (
+    enderecoID int,
+    cep varchar(255),
+    numero varchar(255),
+    complemento varchar(255),
+    bairro varchar(255),
+    usuarioID int,
+    PRIMARY KEY (enderecoID),
+    FOREIGN KEY (usuarioID) REFERENCES usuario(usuarioID)
+);
 
+CREATE TABLE cartao (
+    cartaoID int,
+    numero varchar(255),
+    validade varchar(255),
+    usuarioID int,
+    PRIMARY KEY (cartaoID),
+    FOREIGN KEY (usuarioID) REFERENCES usuario(usuarioID)
+);
 
+CREATE TABLE compra (
+    compraID int,
+    usuarioID int,
+    enderecoEntregaID int,
+    enderecoCobrancaID int,
+    cartaoID int,
+    PRIMARY KEY (compraID),
+    FOREIGN KEY (usuarioID) REFERENCES usuario(usuarioID),
+    FOREIGN KEY (enderecoEntregaID) REFERENCES endereco(enderecoID),
+    FOREIGN KEY (enderecoCobrancaID) REFERENCES endereco(enderecoID),
+    FOREIGN KEY (cartaoID) REFERENCES cartao(cartaoID)
+);
 
-
+CREATE TABLE produto_compra (
+    compraID int,
+    produtoID int,
+    quantidade int,
+    PRIMARY KEY (compraID, produtoID),
+    FOREIGN KEY (compraID) REFERENCES compra(compraID),
+    FOREIGN KEY (produtoID) REFERENCES produto(produtoID)
+);
